a4291beefd293a3b348f6a332c9dab012c4013ec
Jun <jun@jun-900X3T-900X3U>
2018-11-05 22:04:27 +0900

    does not load pages when using browser

--- proxy.py
+++ proxy.py
@@ -1,21 +1,19 @@
+#!/usr/bin/python3
+
 from socket import *
 from urllib.parse import urlparse
 import threading
-import sys; import signal
+import sys
 import argparse
 
-def sig_handler(sig, frame):
-    sock.shutdown(SHUT_RDWR)
-    sock.close()
-    sys.exit(0)
 
-signal.signal(signal.SIGINT, sig_handler)
 
 BUFSIZE = 2048
 TIMEOUT = 5
 HTTP_PORT = 80
 PROXY_PORT = 3128
 CRLF = '\r\n'
+bCRLF = b'\r\n'
 
 parser = argparse.ArgumentParser()
 # in argparse: argument without dash is a positional argument
@@ -24,19 +22,22 @@ parser.add_argument('-mt', action='store_true')
 parser.add_argument('-pc', action='store_true')
 args = parser.parse_args()
 
-cache = {} # dict[url] = HTTPPacket
+
+def sig_handler():
+    sock.shutdown(SHUT_RDWR)
+    sock.close()
+    sys.exit(0)
 
 # Dissect HTTP header into line(first line), header(second line to end), body
 # works
 def parseHTTP(data):
-    data = data.decode()
-    line, data = data[0:data.index(CRLF)], data[data.index(CRLF)+len(CRLF):]
-    data, body = data.split(CRLF+CRLF)
-    body = body.encode()
-    data = data.split(CRLF)
+    line, data = data[0:data.index(bCRLF)], data[data.index(bCRLF)+len(bCRLF):]
+    data, body = data[0:data.index(bCRLF+bCRLF)], data[data.index(bCRLF+bCRLF)+len(bCRLF+bCRLF):]
+    data = data.split(bCRLF)
+    line = line.decode()
 
     header = dict()
-    for field in data:
+    for field in [elt.decode() for elt in data]:
         idx = field.index(':')
         key = field[0:idx]
         value = field[idx+2:]
@@ -93,6 +94,7 @@ def recvData(conn):
     packet.body = body
     return packet.pack()
 
+
 # HTTP packet class
 # Manage packet data and provide related functions
 class HTTPPacket:
@@ -130,13 +132,16 @@ class HTTPPacket:
     def getURL(self):
         return self.line.split(' ')[1]
 
-    # Remove hostname from request packet line
-    def setURL(self):
-        hostname = self.getHeader('Host')
+    def getBodySize(self):
+        return len(self.body)
 
-        new_line = self.line.split(' ')
-        new_line[1] = new_line[1][new_line[1].index(hostname)+len(hostname):]
-        self.line = ' '.join(new_line)
+    # Remove hostname from request packet line
+    #def setURL(self):
+    #    hostname = self.getHeader('Host')
+
+    #    new_line = self.line.split(' ')
+    #    new_line[1] = new_line[1][new_line[1].index(hostname)+len(hostname):]
+    #    self.line = ' '.join(new_line)
     
     def isChunked(self):
         return 'chunked' in self.getHeader('Transfer-Encoding')
@@ -148,33 +153,52 @@ class ProxyThread(threading.Thread):
         super().__init__()
         self.conn = conn  # Client socket
         self.addr = addr  # Client address
+        self.first_run = True
 
     def __del__(self):
-        self.conn.shutdown(SHUT_RDWR)
-        self.conn.close()
+        try:
+            self.conn.shutdown(SHUT_RDWR)
+            self.conn.close()
+        except Exception:
+            pass
     
     # Thread Routine
     def run(self):
         while True:
             try:
+                import pdb; pdb.set_trace()
                 data = recvData(self.conn)
                 req = parseHTTP(data)
+
                 # note: there's also urlunparse(ParseResult)
                 url = urlparse(req.getURL())
     
-                # Remove proxy infomation when doing persistent connection
-                # as there is a limit to number of persistent connection a client (in this case our proxy server)
-                # can maintain at the same time
+                if req.getHeader('Connection').lower() == 'closed': 
+                    print("Connection Closed")
+                    return
 
+                print("requset:")
+                print(*[(k, v) for k, v in zip(req.header, req.header.values())], sep='\n')
+                print('\n\n')
+
+                # Remove proxy infomation when doing persistent connection
+                # https://www.oreilly.com/library/view/http-the-definitive/1565925092/ch04s05.html
                 if args.pc:
-                    req.setURL()
-                    # TODO remove proxy information
-                    pass
+                    if req.getHeader('Connection').lower() != 'keep-alive':
+                        if req.getHeader('Proxy-Connection').lower() == 'keep-alive':
+                            req.setHeader('Connection', 'Keep-Alive')
+                        req.setHeader('Proxy-Connection', '')
+                    else:
+                        req.setHeader('Proxy-Connection', '')
+                else:
+                    req.setHeader('Connection', '')
 
                 # Server connect
                 # and so on...
-                svr = socket(AF_INET, SOCK_STREAM)
-                svr.connect((url.netloc, HTTP_PORT))
+                if self.first_run:
+                    svr = socket(AF_INET, SOCK_STREAM)
+                    svr.connect((url.netloc, HTTP_PORT))
+                    self.first_run = False
     
                 # send a client's request to the server
                 # sendall repeatedly calls send untill buffer is empty or error occurs
@@ -183,27 +207,27 @@ class ProxyThread(threading.Thread):
                 # receive data from the server
                 data = recvData(svr)
                 res = parseHTTP(data)
-                cache[url] = res
                 self.conn.sendall(res.pack())
-                svr.sendall(res.pack())
 
                 # Set content length header
+                res.setHeader('Content-Length', f'{res.getBodySize()}')
     
                 # If support pc, how to do socket and keep-alive?
-
-                if args.pc:
-                    continue
-                else:
-                    raise Exception
     
             except KeyboardInterrupt:
-                print("Keyboard Interrupt...")
-            except socket.timeout:
-                print("Socket Timeout...")
+                print("Child Thread Keyboard Interrupt...")
+            except timeout:
+                print("Socket Timeout. Closing Connection...")
             except Exception as e:
-                pass
-            finally:
-                return
+                print("Exception occured")
+                print(e)
+            if args.pc == False : break
+        print("end of run return")
+        return
+            #finally:
+            #    import pdb; pdb.set_trace()
+            #    print("finally return")
+            #    return
     
 def main():
     try:
@@ -213,6 +237,7 @@ def main():
         sock.bind(('0.0.0.0', args.port))
         sock.listen(20)
         print('Proxy Server started on port %d' % args.port)
+        n = 0
         
         while True:
             # Client connect
@@ -222,12 +247,20 @@ def main():
             print("new connection from " + str(addr))
             # Start Handling
             pt = ProxyThread(conn, addr)
+            pt.daemon = True
             pt.start()
             if args.mt == False:
                 pt.join()
+            n += 1
+            print(f'{n} threads have been created')
     except Exception as e:
         print(e)
         pass
+    except KeyboardInterrupt:
+        print("Main Thread Keyboard Interrupt...")
+        pass
+    finally:
+        sig_handler()
 
 
 if __name__ == '__main__':
